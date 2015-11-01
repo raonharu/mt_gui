54168a602460b456558289c824b9d89cdbbaac28
raonharu <liminia52@gmail.com>
2015-11-02 01:01:09 +0900

    first
    first

---
+++ mt_gui/mt_guiDlg.h
@@ -0,0 +1,31 @@
+
+// mt_guiDlg.h : 헤더 파일
+//
+
+#pragma once
+
+
+// Cmt_guiDlg 대화 상자
+class Cmt_guiDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	Cmt_guiDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+	enum { IDD = IDD_MT_GUI_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
